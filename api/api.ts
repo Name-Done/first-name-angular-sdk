2b556429683bd237bc5a726a2e2c32f6f9c8cc82
jonURT <123364219+jonURT@users.noreply.github.com>
2023-08-16 17:00:27 +0000

    Update SDK

--- api/api.ts
+++ api/api.ts
@@ -4,4 +4,6 @@ export * from './familyName.service';
 import { FamilyNameService } from './familyName.service';
 export * from './givenName.service';
 import { GivenNameService } from './givenName.service';
-export const APIS = [EmailDomainNameService, FamilyNameService, GivenNameService];
+export * from './topLevelDomainName.service';
+import { TopLevelDomainNameService } from './topLevelDomainName.service';
+export const APIS = [EmailDomainNameService, FamilyNameService, GivenNameService, TopLevelDomainNameService];
